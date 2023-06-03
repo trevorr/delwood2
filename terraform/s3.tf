6842ece5ad892a0ee2367b0ddc6743693fb74f8d
Trevor Robinson <trevor@scurrilous.com>
2023-06-03 16:36:36 -0500

    Add PublicReadGetObject bucket policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -15,6 +15,29 @@ resource "aws_s3_bucket_public_access_block" "delwood2" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_policy" "delwood2" {
+  bucket = aws_s3_bucket.delwood2.id
+  policy = data.aws_iam_policy_document.delwood2.json
+}
+
+data "aws_iam_policy_document" "delwood2" {
+  statement {
+    sid = "PublicReadGetObject"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.delwood2.arn}/*",
+    ]
+  }
+}
+
 resource "aws_s3_bucket_website_configuration" "delwood2" {
   bucket = aws_s3_bucket.delwood2.id
 
